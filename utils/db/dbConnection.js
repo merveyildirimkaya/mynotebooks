8e883aa16a9930069b08f7955268d897b1c465ab
merveyildirimkaya <89215658+merveyildirimkaya@users.noreply.github.com>
2022-09-05 16:35:08 +0300

    heroku configs

--- utils/db/dbConnection.js
+++ utils/db/dbConnection.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const connectDatabase = async()=>{
     
-    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    await mongoose.connect(process.env.DB_CONNECTION_STRING ||"mongodb://localhost:27017/mynotebook" );
 
 }
 
