fd31d9871c837cba45f276cfb5e23239954de535
merveyildirimkaya <89215658+merveyildirimkaya@users.noreply.github.com>
2022-09-05 23:59:46 +0300

    little details

--- utils/middleware/accessForCards.js
+++ utils/middleware/accessForCards.js
@@ -5,7 +5,7 @@ const createError = require('http-errors')
 const accessForCards = async (req,res,next)=>{
 
     try {
-        const card = await Card.findById({_id:req.params.cardId})
+        const card = await Card.findById({_id:req.params.cardId},'-__v')
         const notebook= await Notebook.findById({_id:card.notebookId})
         if((req.user._id).equals((notebook.userId).toString())){
             req.card = card
