ff6af55e13ecc8821684cdabb8b8eaadd4e2531d
merveyildirimkaya <89215658+merveyildirimkaya@users.noreply.github.com>
2022-09-05 17:28:21 +0300

    error handling

--- utils/db/dbConnection.js
+++ utils/db/dbConnection.js
@@ -3,8 +3,9 @@ require("dotenv").config()
 
 const connectDatabase = async()=>{
     
-    await mongoose.connect(process.env.DB_CONNECTION_STRING ||"mongodb://localhost:27017/mynotebook" );
+    await mongoose.connect(process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/mynotebook",{ useNewUrlParser: true, useUnifiedTopology: true});
 
 }
 
 module.exports = {connectDatabase}
+
